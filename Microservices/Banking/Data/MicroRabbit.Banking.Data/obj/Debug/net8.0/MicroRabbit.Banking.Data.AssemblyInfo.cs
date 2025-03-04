c54f5cbe66b2a151481b07a86ce22eac2b28149f
Issacboox <nurarat0024@gmail.com>
2025-03-04 16:37:54 +0700

    [Implement] : base transfer

--- Microservices/Banking/Data/MicroRabbit.Banking.Data/obj/Debug/net8.0/MicroRabbit.Banking.Data.AssemblyInfo.cs
+++ Microservices/Banking/Data/MicroRabbit.Banking.Data/obj/Debug/net8.0/MicroRabbit.Banking.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroRabbit.Banking.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eeb2a4f6c7f8370c7ec530438132ecafc87f024")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62487a0c0b72597030cdd17c723ff5df23d4f612")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroRabbit.Banking.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroRabbit.Banking.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
